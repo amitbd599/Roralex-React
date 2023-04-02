c43097b19341a14ef9064bdb6f11c61efbcc9891
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-04-02 23:46:00 +0600

    Blog Section version three add

--- src/components/BlogSectionThree.js
+++ src/components/BlogSectionThree.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { FaCalendarAlt, FaUserAlt } from "react-icons/fa";
-
+import { FaCalendarAlt, FaRegCalendarAlt, FaUserAlt } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
 const BlogSectionThree = () => {
   return (
     <>
@@ -30,208 +31,232 @@ const BlogSectionThree = () => {
             <div className='row'>
               <div className='swiper swiper-container blog-version-three'>
                 <div className='swiper-wrapper'>
-                  <div className='swiper-slide'>
-                    <div className='wrapper shadow'>
-                      <div className='img-file'>
-                        <img
-                          className='img-fluid'
-                          src='./assets/img/blog/blog-1.png'
-                          alt=''
-                        />
-                      </div>
-                      <div className='inner-file'>
-                        <a href='blog-details.html'>
-                          <h2>
-                            Building construction is the process of structure
-                          </h2>
-                        </a>
-                        <p>
-                          The vast majority of building construction projects
-                          are small renovations, such as the addition of a room,
-                          or renovation of a bathroom.
-                        </p>
-                        <div className='footer-icons'>
-                          <div>
-                            <span>
-                              <i className='fa-solid fa-user' />
-                            </span>
-                            <span>
-                              By{" "}
-                              <strong className='theme-color'>Alex Jhon</strong>
-                            </span>
+                  <Swiper
+                    // install Swiper modules
+                    modules={[Pagination]}
+                    spaceBetween={0}
+                    slidesPerView={3}
+                    slidesPerGroup={2}
+                    speed={1000}
+                    pagination={{ clickable: true }}
+                    autoplay={{ delay: 6000 }}
+                    breakpoints={{
+                      0: {
+                        slidesPerView: 1,
+                        slidesPerGroup: 1,
+                      },
+                      500: {
+                        slidesPerView: 1,
+                        slidesPerGroup: 1,
+                      },
+                      768: {
+                        slidesPerView: 2,
+                      },
+                      992: {
+                        slidesPerView: 3,
+                        slidesPerGroup: 2,
+                      },
+                      1200: {
+                        slidesPerView: 3,
+                        slidesPerGroup: 2,
+                      },
+                      1400: {
+                        slidesPerView: 3,
+                        slidesPerGroup: 2,
+                      },
+                    }}
+                  >
+                    <SwiperSlide>
+                      <div>
+                        <div className='wrapper shadow'>
+                          <div className='img-file'>
+                            <img
+                              className='img-fluid'
+                              src='./assets/img/blog/blog-1.png'
+                              alt=''
+                            />
                           </div>
-                          <div>
-                            <span>
-                              <i className='fa-solid fa-timer' />
-                            </span>
-                            <span>4 years ago</span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className='swiper-slide'>
-                    <div className='wrapper shadow'>
-                      <div className='img-file'>
-                        <img
-                          className='img-fluid'
-                          src='./assets/img/blog/blog-2.png'
-                          alt=''
-                        />
-                      </div>
-                      <div className='inner-file'>
-                        <a href='blog-details.html'>
-                          <h2>There is a wide range of building products.</h2>
-                        </a>
-                        <p>
-                          The vast majority of building construction projects
-                          are small renovations, such as the addition of a room,
-                          or renovation of a bathroom.
-                        </p>
-                        <div className='footer-icons'>
-                          <div>
-                            <span>
-                              <i className='fa-solid fa-user' />
-                            </span>
-                            <span>
-                              By{" "}
-                              <strong className='theme-color'>Alex Jhon</strong>
-                            </span>
-                          </div>
-                          <div>
-                            <span>
-                              <i className='fa-solid fa-timer' />
-                            </span>
-                            <span>4 years ago</span>
+                          <div className='inner-file'>
+                            <a href='blog-details.html'>
+                              <h2>
+                                Building construction is the process of
+                                structure
+                              </h2>
+                            </a>
+                            <p>
+                              The vast majority of building construction
+                              projects are small renovations, such as the
+                              addition of a room, or renovation of a bathroom.
+                            </p>
+                            <div className='footer-icons'>
+                              <div className='inner'>
+                                <FaUserAlt />
+                                By{" "}
+                                <strong className='theme-color'>
+                                  Alex Jhon
+                                </strong>
+                              </div>
+                              <div className='inner'>
+                                <FaRegCalendarAlt />4 years ago
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                  <div className='swiper-slide'>
-                    <div className='wrapper shadow'>
-                      <div className='img-file'>
-                        <img
-                          className='img-fluid'
-                          src='./assets/img/blog/blog-3.png'
-                          alt=''
-                        />
-                      </div>
-                      <div className='inner-file'>
-                        <a href='blog-details.html'>
-                          <h2>
-                            Construction today is a significant industrial.
-                          </h2>
-                        </a>
-                        <p>
-                          The vast majority of building construction projects
-                          are small renovations, such as the addition of a room,
-                          or renovation of a bathroom.
-                        </p>
-                        <div className='footer-icons'>
-                          <div>
-                            <span>
-                              <i className='fa-solid fa-user' />
-                            </span>
-                            <span>
-                              By{" "}
-                              <strong className='theme-color'>Alex Jhon</strong>
-                            </span>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div>
+                        <div className='wrapper shadow'>
+                          <div className='img-file'>
+                            <img
+                              className='img-fluid'
+                              src='./assets/img/blog/blog-2.png'
+                              alt=''
+                            />
                           </div>
-                          <div>
-                            <span>
-                              <i className='fa-solid fa-timer' />
-                            </span>
-                            <span>4 years ago</span>
+                          <div className='inner-file'>
+                            <a href='blog-details.html'>
+                              <h2>
+                                There is a wide range of building products.
+                              </h2>
+                            </a>
+                            <p>
+                              The vast majority of building construction
+                              projects are small renovations, such as the
+                              addition of a room, or renovation of a bathroom.
+                            </p>
+                            <div className='footer-icons'>
+                              <div className='inner'>
+                                <FaUserAlt />
+                                By{" "}
+                                <strong className='theme-color'>
+                                  Alex Jhon
+                                </strong>
+                              </div>
+                              <div className='inner'>
+                                <FaRegCalendarAlt />4 years ago
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                  <div className='swiper-slide'>
-                    <div className='wrapper shadow'>
-                      <div className='img-file'>
-                        <img
-                          className='img-fluid'
-                          src='./assets/img/blog/blog-4.png'
-                          alt=''
-                        />
-                      </div>
-                      <div className='inner-file'>
-                        <a href='blog-details.html'>
-                          <h2>
-                            A tent illustrates basic elements of environmental.
-                          </h2>
-                        </a>
-                        <p>
-                          The vast majority of building construction projects
-                          are small renovations, such as the addition of a room,
-                          or renovation of a bathroom.
-                        </p>
-                        <div className='footer-icons'>
-                          <div>
-                            <span>
-                              <i className='fa-solid fa-user' />
-                            </span>
-                            <span>
-                              By{" "}
-                              <strong className='theme-color'>Alex Jhon</strong>
-                            </span>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div>
+                        <div className='wrapper shadow'>
+                          <div className='img-file'>
+                            <img
+                              className='img-fluid'
+                              src='./assets/img/blog/blog-3.png'
+                              alt=''
+                            />
                           </div>
-                          <div>
-                            <span>
-                              <i className='fa-solid fa-timer' />
-                            </span>
-                            <span>4 years ago</span>
+                          <div className='inner-file'>
+                            <a href='blog-details.html'>
+                              <h2>
+                                Construction today is a significant industrial.
+                              </h2>
+                            </a>
+                            <p>
+                              The vast majority of building construction
+                              projects are small renovations, such as the
+                              addition of a room, or renovation of a bathroom.
+                            </p>
+                            <div className='footer-icons'>
+                              <div className='inner'>
+                                <FaUserAlt />
+                                By{" "}
+                                <strong className='theme-color'>
+                                  Alex Jhon
+                                </strong>
+                              </div>
+                              <div className='inner'>
+                                <FaRegCalendarAlt />4 years ago
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                  <div className='swiper-slide'>
-                    <div className='wrapper shadow'>
-                      <div className='img-file'>
-                        <img
-                          className='img-fluid'
-                          src='./assets/img/blog/blog-5.png'
-                          alt=''
-                        />
-                      </div>
-                      <div className='inner-file'>
-                        <a href='blog-details.html'>
-                          <h2>
-                            People no traveled in search of game or herds.
-                          </h2>
-                        </a>
-                        <p>
-                          The vast majority of building construction projects
-                          are small renovations, such as the addition of a room,
-                          or renovation of a bathroom.
-                        </p>
-                        <div className='footer-icons'>
-                          <div>
-                            <span>
-                              <i className='fa-solid fa-user' />
-                            </span>
-                            <span>
-                              By{" "}
-                              <strong className='theme-color'>Alex Jhon</strong>
-                            </span>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div>
+                        <div className='wrapper shadow'>
+                          <div className='img-file'>
+                            <img
+                              className='img-fluid'
+                              src='./assets/img/blog/blog-4.png'
+                              alt=''
+                            />
                           </div>
-                          <div>
-                            <span>
-                              <i className='fa-solid fa-timer' />
-                            </span>
-                            <span>4 years ago</span>
+                          <div className='inner-file'>
+                            <a href='blog-details.html'>
+                              <h2>
+                                A tent illustrates basic elements of
+                                environmental.
+                              </h2>
+                            </a>
+                            <p>
+                              The vast majority of building construction
+                              projects are small renovations, such as the
+                              addition of a room, or renovation of a bathroom.
+                            </p>
+                            <div className='footer-icons'>
+                              <div className='inner'>
+                                <FaUserAlt />
+                                By{" "}
+                                <strong className='theme-color'>
+                                  Alex Jhon
+                                </strong>
+                              </div>
+                              <div className='inner'>
+                                <FaRegCalendarAlt />4 years ago
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div>
+                        <div className='wrapper shadow'>
+                          <div className='img-file'>
+                            <img
+                              className='img-fluid'
+                              src='./assets/img/blog/blog-5.png'
+                              alt=''
+                            />
+                          </div>
+                          <div className='inner-file'>
+                            <a href='blog-details.html'>
+                              <h2>
+                                People no traveled in search of game or herds.
+                              </h2>
+                            </a>
+                            <p>
+                              The vast majority of building construction
+                              projects are small renovations, such as the
+                              addition of a room, or renovation of a bathroom.
+                            </p>
+                            <div className='footer-icons'>
+                              <div className='inner'>
+                                <FaUserAlt />
+                                By{" "}
+                                <strong className='theme-color'>
+                                  Alex Jhon
+                                </strong>
+                              </div>
+                              <div className='inner'>
+                                <FaRegCalendarAlt />4 years ago
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                  </Swiper>
                 </div>
               </div>
               {/* If we need pagination */}
-              <div className='blog-version-three-pagination' />
             </div>
           </div>
         </div>
