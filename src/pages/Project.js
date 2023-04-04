78e3fc89992336321b7b46c29c76732b76a0bbf6
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-04-04 20:52:51 +0600

    Project add

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,28 +1,36 @@
-import React from "react";
-import HeaderOne from "../components/HeaderOne";
-import BreadCrumb from "../elements/BreadCrumb";
-import LatestProjectsTwo from "../components/LatestProjectsTwo";
-import OurClientOne from "../components/OurClientOne";
-import FooterOne from "../components/FooterOne";
+import React, { Fragment, Suspense } from "react";
+import Preloader from "../elements/Preloader";
+
+const HeaderOne = React.lazy(() => import("../components/HeaderOne"));
+const BreadCrumb = React.lazy(() => import("../elements/BreadCrumb"));
+const LatestProjectsTwo = React.lazy(() =>
+  import("../components/LatestProjectsTwo")
+);
+const OurClientOne = React.lazy(() => import("../components/OurClientOne"));
+const FooterOne = React.lazy(() => import("../components/FooterOne"));
 const Project = () => {
   return (
     <>
-      {/* Header One */}
-      <HeaderOne />
+      <Fragment>
+        <Suspense fallback={<Preloader />}>
+          {/* Header One */}
+          <HeaderOne />
 
-      {/* Bread Crumb */}
-      <BreadCrumb title={"Project"} />
+          {/* Bread Crumb */}
+          <BreadCrumb title={"Project"} />
 
-      {/* LatestProjectsTwo */}
-      <LatestProjectsTwo />
+          {/* LatestProjectsTwo */}
+          <LatestProjectsTwo />
 
-      {/* Our Client One */}
-      <section className='ourclient-section-version-one bg-white py-80'>
-        <OurClientOne />
-      </section>
+          {/* Our Client One */}
+          <section className='ourclient-section-version-one bg-white py-80'>
+            <OurClientOne />
+          </section>
 
-      {/* Footer One */}
-      <FooterOne />
+          {/* Footer One */}
+          <FooterOne />
+        </Suspense>
+      </Fragment>
     </>
   );
 };
