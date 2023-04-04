3aa18b03903eae9e25edba842eee65eb35dc2323
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-04-04 20:31:58 +0600

    HomeThree add

--- src/pages/HomeThree.js
+++ src/pages/HomeThree.js
@@ -1,65 +1,86 @@
-import React from "react";
-import AboutThree from "../components/AboutThree";
-import BlogSectionThree from "../components/BlogSectionThree";
-import ClientReviewThree from "../components/ClientReviewThree";
-import FooterThree from "../components/FooterThree";
-import HeaderThree from "../components/HeaderThree";
-import LatestProjectsThree from "../components/LatestProjectsThree";
-import NewsLetterThree from "../components/NewsLetterThree";
-import OurClientThree from "../components/OurClientThree";
-import OurPlanPriceThree from "../components/OurPlanPriceThree";
-import ProjectBackgroundOverlayThree from "../components/ProjectBackgroundOverlayThree";
-import ServiceListThree from "../components/ServiceListThree";
-import SliderThree from "../components/SliderThree";
-import WhyChooseUsThree from "../components/WhyChooseUsThree";
-import OurTeamThree from "../components/OurTeamThree";
+import React, { Fragment, Suspense } from "react";
+import Preloader from "../elements/Preloader";
 
+const AboutThree = React.lazy(() => import("../components/AboutThree"));
+const BlogSectionThree = React.lazy(() =>
+  import("../components/BlogSectionThree")
+);
+const ClientReviewThree = React.lazy(() =>
+  import("../components/ClientReviewThree")
+);
+const FooterThree = React.lazy(() => import("../components/FooterThree"));
+const HeaderThree = React.lazy(() => import("../components/HeaderThree"));
+const LatestProjectsThree = React.lazy(() =>
+  import("../components/LatestProjectsThree")
+);
+const NewsLetterThree = React.lazy(() =>
+  import("../components/NewsLetterThree")
+);
+const OurClientThree = React.lazy(() => import("../components/OurClientThree"));
+const OurPlanPriceThree = React.lazy(() =>
+  import("../components/OurPlanPriceThree")
+);
+const ProjectBackgroundOverlayThree = React.lazy(() =>
+  import("../components/ProjectBackgroundOverlayThree")
+);
+const ServiceListThree = React.lazy(() =>
+  import("../components/ServiceListThree")
+);
+const SliderThree = React.lazy(() => import("../components/SliderThree"));
+const WhyChooseUsThree = React.lazy(() =>
+  import("../components/WhyChooseUsThree")
+);
+const OurTeamThree = React.lazy(() => import("../components/OurTeamThree"));
 const HomeThree = () => {
   return (
     <>
-      {/* Header Three */}
-      <HeaderThree />
+      <Fragment>
+        <Suspense fallback={<Preloader />}>
+          {/* Header Three */}
+          <HeaderThree />
 
-      {/* Slider Three */}
-      <SliderThree />
+          {/* Slider Three */}
+          <SliderThree />
 
-      {/* About Three */}
-      <AboutThree />
+          {/* About Three */}
+          <AboutThree />
 
-      {/* Service List Three */}
-      <ServiceListThree />
+          {/* Service List Three */}
+          <ServiceListThree />
 
-      {/* Why Choose Us Three */}
-      <WhyChooseUsThree />
+          {/* Why Choose Us Three */}
+          <WhyChooseUsThree />
 
-      {/* Project Background Overlay Three */}
-      <ProjectBackgroundOverlayThree />
+          {/* Project Background Overlay Three */}
+          <ProjectBackgroundOverlayThree />
 
-      {/* Our Team Three */}
-      <section className='ourTeam-section-version-three bg-white pt-200 pb-60'>
-        <OurTeamThree />
-      </section>
+          {/* Our Team Three */}
+          <section className='ourTeam-section-version-three bg-white pt-200 pb-60'>
+            <OurTeamThree />
+          </section>
 
-      {/* Latest Projects Three */}
-      <LatestProjectsThree />
+          {/* Latest Projects Three */}
+          <LatestProjectsThree />
 
-      {/* Client Review Three */}
-      <ClientReviewThree />
+          {/* Client Review Three */}
+          <ClientReviewThree />
 
-      {/* Blog Section Three */}
-      <BlogSectionThree />
+          {/* Blog Section Three */}
+          <BlogSectionThree />
 
-      {/* Our Plan Price Three */}
-      <OurPlanPriceThree />
+          {/* Our Plan Price Three */}
+          <OurPlanPriceThree />
 
-      {/* News Letter Three */}
-      <NewsLetterThree />
+          {/* News Letter Three */}
+          <NewsLetterThree />
 
-      {/* Our Client Three */}
-      <OurClientThree />
+          {/* Our Client Three */}
+          <OurClientThree />
 
-      {/* Footer Three */}
-      <FooterThree />
+          {/* Footer Three */}
+          <FooterThree />
+        </Suspense>
+      </Fragment>
     </>
   );
 };
