60c6c736ad0061ad9ed83b39f87e83811136a375
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-04-04 20:46:52 +0600

    Blog add

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,23 +1,28 @@
-import React from "react";
-import HeaderOne from "../components/HeaderOne";
-import BreadCrumb from "../elements/BreadCrumb";
-import BlogGroup from "../components/BlogGroup";
-import FooterOne from "../components/FooterOne";
+import React, { Fragment, Suspense } from "react";
+import Preloader from "../elements/Preloader";
 
+const HeaderOne = React.lazy(() => import("../components/HeaderOne"));
+const BreadCrumb = React.lazy(() => import("../elements/BreadCrumb"));
+const BlogGroup = React.lazy(() => import("../components/BlogGroup"));
+const FooterOne = React.lazy(() => import("../components/FooterOne"));
 const Blog = () => {
   return (
     <>
-      {/* Header One */}
-      <HeaderOne />
+      <Fragment>
+        <Suspense fallback={<Preloader />}>
+          {/* Header One */}
+          <HeaderOne />
 
-      {/* Bread Crumb */}
-      <BreadCrumb title={"Blog"} />
+          {/* Bread Crumb */}
+          <BreadCrumb title={"Blog"} />
 
-      {/* Blog Group */}
-      <BlogGroup />
+          {/* Blog Group */}
+          <BlogGroup />
 
-      {/* Footer One */}
-      <FooterOne />
+          {/* Footer One */}
+          <FooterOne />
+        </Suspense>
+      </Fragment>
     </>
   );
 };
