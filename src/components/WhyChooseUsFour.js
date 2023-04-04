72673eb37953a05ca7fe2e338121c662e659b193
amitbd591 <122339814+amitbd591@users.noreply.github.com>
2023-04-04 10:17:08 +0600

    About Us page add with tabs

--- src/components/WhyChooseUsFour.js
+++ src/components/WhyChooseUsFour.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { FaCheck } from "react-icons/fa";
-
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 const WhyChooseUsFour = () => {
   return (
     <>
       {/* Why Choose Us Section version Four start */}
-      <section className='whyChooseUs-section-version-four bg-gray py-100 '>
+      <section className='whyChooseUs-section-version-four bg-gray py-100 mt-60'>
         {/* Header Intro Version One */}
         <div className='header-intro-version-one'>
           <div className='container'>
@@ -35,121 +35,151 @@ const WhyChooseUsFour = () => {
             <div className='row'>
               <div className='col-xl-7'>
                 <div className='left-side'>
-                  <div className='tabs-inner'>
-                    <ul className='tabs'>
-                      <li>
-                        <a href='#tab1'>Administrator</a>
-                      </li>
-                      <li>
-                        <a href='#tab2'>Services</a>
-                      </li>
-                      <li>
-                        <a href='#tab3'>Features</a>
-                      </li>
-                    </ul>
-                    <section className='tab_content_wrapper'>
-                      <div className='tab_content' id='tab1'>
-                        <div className='wrapper'>
-                          <div className='left'>
-                            <div className='img-file'>
-                              <img
-                                src='./assets/img/about-us/about-7.png'
-                                alt=''
-                              />
+                  <Tabs>
+                    <div className='tabs-inner'>
+                      <TabList>
+                        {/* <ul className='tabs'>
+                          <li>
+                            <Tab>
+                              <span>Administrator</span>
+                            </Tab>
+                          </li>
+                          <li>
+                            <Tab>
+                              <span>Services</span>
+                            </Tab>
+                          </li>
+                          <li>
+                            <Tab>
+                              <span>Features</span>
+                            </Tab>
+                          </li>
+                        </ul> */}
+                        <Tab>
+                          <span>Administrator</span>
+                        </Tab>
+                        <Tab>
+                          <span>Services</span>
+                        </Tab>
+                        <Tab>
+                          <span>Features</span>
+                        </Tab>
+                      </TabList>
+                      <section className='tab_content_wrapper'>
+                        <TabPanel>
+                          <div className='tab_content'>
+                            <div className='wrapper'>
+                              <div className='left'>
+                                <div className='img-file'>
+                                  <img
+                                    src='./assets/img/about-us/about-7.png'
+                                    alt=''
+                                  />
+                                </div>
+                              </div>
+                              <div className='right'>
+                                <h2>Best Feature Of Building construction </h2>
+                                <ul>
+                                  <li>
+                                    <FaCheck />
+                                    Design Details and Full Feasibility Study.
+                                  </li>
+                                  <li>
+                                    <FaCheck />
+                                    Incorporation of Permits into Design.
+                                  </li>
+                                  <li>
+                                    <FaCheck />
+                                    Choosing General Contractor &amp;
+                                    Construction.
+                                  </li>
+                                  <li>
+                                    <FaCheck />
+                                    Research, Surveys, and Sketches.
+                                  </li>
+                                </ul>
+                              </div>
                             </div>
                           </div>
-                          <div className='right'>
-                            <h2>Best Feature Of Building construction </h2>
-                            <ul>
-                              <li>
-                                <FaCheck />
-                                Design Details and Full Feasibility Study.
-                              </li>
-                              <li>
-                                <FaCheck />
-                                Incorporation of Permits into Design.
-                              </li>
-                              <li>
-                                <FaCheck />
-                                Choosing General Contractor &amp; Construction.
-                              </li>
-                              <li>
-                                <FaCheck />
-                                Research, Surveys, and Sketches.
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                      </div>
-                      <div className='tab_content' id='tab2'>
-                        <div className='wrapper'>
-                          <div className='left'>
-                            <div className='img-file'>
-                              <img
-                                src='./assets/img/about-us/about-8.png'
-                                alt=''
-                              />
+                        </TabPanel>
+
+                        <TabPanel>
+                          <div className='tab_content'>
+                            <div className='wrapper'>
+                              <div className='left'>
+                                <div className='img-file'>
+                                  <img
+                                    src='./assets/img/about-us/about-8.png'
+                                    alt=''
+                                  />
+                                </div>
+                              </div>
+                              <div className='right'>
+                                <h2>Egyptian stone diffused the eastern.</h2>
+                                <ul>
+                                  <li>
+                                    <FaCheck />
+                                    The first surviving construction drawings.
+                                  </li>
+                                  <li>
+                                    <FaCheck />
+                                    In contrast to stone technology remained.
+                                  </li>
+                                  <li>
+                                    <FaCheck />
+                                    Developed the final of columns and moldings.
+                                  </li>
+                                  <li>
+                                    <FaCheck />A technique borrowed from stone
+                                    construction.
+                                  </li>
+                                </ul>
+                              </div>
                             </div>
                           </div>
-                          <div className='right'>
-                            <h2>Egyptian stone diffused the eastern.</h2>
-                            <ul>
-                              <li>
-                                <FaCheck />
-                                The first surviving construction drawings.
-                              </li>
-                              <li>
-                                <FaCheck />
-                                In contrast to stone technology remained.
-                              </li>
-                              <li>
-                                <FaCheck />
-                                Developed the final of columns and moldings.
-                              </li>
-                              <li>
-                                <FaCheck />A technique borrowed from stone
-                                construction.
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                      </div>
-                      <div className='tab_content' id='tab3'>
-                        <div className='wrapper'>
-                          <div className='left'>
-                            <div className='img-file'>
-                              <img
-                                src='./assets/img/about-us/about-9.png'
-                                alt=''
-                              />
+                        </TabPanel>
+
+                        <TabPanel>
+                          <div className='tab_content'>
+                            <div className='wrapper'>
+                              <div className='left'>
+                                <div className='img-file'>
+                                  <img
+                                    src='./assets/img/about-us/about-9.png'
+                                    alt=''
+                                  />
+                                </div>
+                              </div>
+                              <div className='right'>
+                                <h2>Although stone technology confined. </h2>
+                                <ul>
+                                  <li>
+                                    <FaCheck />
+                                    Perhaps the spectacular building
+                                    achievement.
+                                  </li>
+                                  <li>
+                                    <FaCheck />
+                                    It was a huge stone tower nearly as the
+                                    Great.
+                                  </li>
+                                  <li>
+                                    <FaCheck />
+                                    Another example of a new stone technology.
+                                  </li>
+                                  <li>
+                                    <FaCheck />
+                                    Thus it does not take full advantage of the
+                                    great.
+                                  </li>
+                                </ul>
+                              </div>
                             </div>
                           </div>
-                          <div className='right'>
-                            <h2>Although stone technology confined. </h2>
-                            <ul>
-                              <li>
-                                <FaCheck />
-                                Perhaps the spectacular building achievement.
-                              </li>
-                              <li>
-                                <FaCheck />
-                                It was a huge stone tower nearly as the Great.
-                              </li>
-                              <li>
-                                <FaCheck />
-                                Another example of a new stone technology.
-                              </li>
-                              <li>
-                                <FaCheck />
-                                Thus it does not take full advantage of the
-                                great.
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                      </div>
-                    </section>
-                  </div>
+                        </TabPanel>
+                      </section>
+                    </div>
+                  </Tabs>
                 </div>
               </div>
               <div className='col-xl-5'>
